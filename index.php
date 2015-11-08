37a3983715ebb831470bfda89b71a97feed0fcea
Jenny Swift <cheezyspaghetti@gmail.com>
2015-11-09 07:49:31 +1100

    Include file for navbar

--- index.php
+++ index.php
@@ -12,53 +12,8 @@
 //    $smarty ->display('index.tpl');
     ?>  -->
 
-  	<div class="navbar navbar-default navbar-fixed-top nav-color" role="navigation">
-    <div class="container">
-  	<div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.html"><span class="menu-color">CS Nation</span></a>
-    </div>
+    <?php include ('php/includes/navbar.php'); ?>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="navbar-collapse">
-      <ul class="nav navbar-nav">
-        <li class="dropdown"><!--Dropdown menu for Courses begins-->
-          <a href="#" class="dropdown-toggle menu-color" data-toggle="dropdown" role="button" aria-expanded="false"><span class="menu-color">Courses</span><span class="caret menu-color"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">CS Nation</a></li>
-            <li><a href="#">Test1</a></li>
-            <li><a href="#">Test2</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Test3</a></li>
-          </ul>
-        </li> <!--Dropdown menu for Courses ends-->
-
-    <li class="dropdown"> <!--Dropdown menu for About Us Begins-->
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="menu-color">About Us</span><span class="caret menu-color"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Test1</a></li>
-            <li><a href="#">Test2</a></li>
-            <li><a href="#">Test3</a></li>
-            <li class="divider"></li>
-            <li><a href="about_us.html">Contact Us</a></li>
-          </ul>
-        </li>	<!--Dropdown menu for About Us ends-->
-      </ul>
-      <form class="navbar-form navbar-right" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default button-color"><span class="menu-color">Search</span></button>
-      </form>
-
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container -->
-</div>
 	<div class="container">
  	<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
 
